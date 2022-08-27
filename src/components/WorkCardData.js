2283b00fb28ad89d20320fd46ea1f0959ba8bf8b
enjoyghz <adamghosi03@gmail.com>
2022-08-27 12:32:28 +0700

    ProjectCard

---
+++ src/components/WorkCardData.js
@@ -0,0 +1,27 @@
+import pro1 from "../assets/project1.jpg";
+import pro2 from "../assets/project2.jpg";
+import pro3 from "../assets/project3.jpg";
+
+
+const ProjectCardData = [
+    {
+        imgsrc: pro1,
+        title: "SI KORPRI (Sistem Informasi Korpri Kab. BLora)",
+        text: "SI KORPRI (Sistem Informasi KORPRI) ini adalah website yang saya kembangkan yang sekarang telah digunakan oleh Kantor KORPRI Kab. Blora. Merupakan web pengelolaan anggota dan keuangan pegawai KORPRI. (Gambar hanya pemanis)",
+        view: "http://sikorpri.blorakab.go.id/"
+    },
+    {
+        imgsrc: pro2,
+        title: "Landing Page Personal Portfolio",
+        text: "Ini adalah contoh dari LandingPage yang telah saya buat dengan React.js. Sangat dinamis dan waktu loading halaman sangat cepat dan responsive.(Gambar hanya pemanis)",
+        view: "https://github.com/enjoyghozi/portfolioreactjs"
+    },
+    {
+        imgsrc: pro3,
+        title: "Teman Apotik",
+        text: "Ini adalah project bersama yang saya kembangkan dengan teman saya sewaktu kuliah IT di Universitas Muhammadiyah Yogyakarta. (Gambar hanya pemanis)",
+        view: "https://github.com/enjoyghozi/temanapotik"
+    },
+];
+
+export default ProjectCardData
